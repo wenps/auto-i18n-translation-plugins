b903257e068a7666c2a78802f7cbbcfb89763c3d
Caleb-Xu <noname1427968964@outlook.com>
2025-03-26 15:14:03 +0800

    chore: 解除dist文件夹的git版本管理

--- packages/vitePluginsAutoI18n/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { OptionInfo } from 'auto-i18n-plugin-core';
-export * from 'auto-i18n-plugin-core';
-
-declare function vitePluginsAutoI18n(optionInfo: OptionInfo): any;
-
-export { vitePluginsAutoI18n as default };
